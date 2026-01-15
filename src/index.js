b94d8c5a394c21d89e9bfa3b1bd0f14fc4fe62e3
Syaz <24050099@myrp.edu.sg>
2026-01-15 17:15:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { Routes, Route } from "react-router-dom";
 
-import "./index.css";
-import App from "./components/App";
+import Home from "./Home";
+import About from "./About";
+import Categories from "./Categories";
+import Category from "./Category";
+import Header from "./Header";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+function App() {
+  return (
+    <div className="app">
+      <Header />
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
+        <Route path="about" element={<About />} />
+        <Route path="categories" element={<Categories />}>
+          <Route path=":catId" element={<Category />} />
+        </Route>
+        <Route
+          path="*"
+          element={<h1 className="not-found">Page Not Found</h1>}
+        />
+      </Routes>
+
+      <footer className="container">
+        &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
