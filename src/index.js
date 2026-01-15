5daa198bc1c5a1e8b05b0976e3ab1b806b907b11
Syaz <24050099@myrp.edu.sg>
2026-01-15 17:19:22 +0800

    Updated mistake

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import Home from "./Home";
-import About from "./About";
-import Categories from "./Categories";
-import Category from "./Category";
-import Header from "./Header";
+import "./index.css";
+import App from "./components/App";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-      <Routes>
-        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
-        <Route path="about" element={<About />} />
-        <Route path="categories" element={<Categories />}>
-          <Route path=":catId" element={<Category />} />
-        </Route>
-        <Route
-          path="*"
-          element={<h1 className="not-found">Page Not Found</h1>}
-        />
-      </Routes>
-
-      <footer className="container">
-        &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
-      </footer>
-    </div>
-  );
-}
-
-export default App;
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
